23f98820e974145a6a030fca4cd1c64186ef0b2c
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-12-07 21:42:00 +0000

    changed secret key

--- shop_chichi/settings.py
+++ shop_chichi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y&_3o=s-1wi)%+2gk+6yw=8iw=_$rl!7j#45(8+vsm+-)-_x&w'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
