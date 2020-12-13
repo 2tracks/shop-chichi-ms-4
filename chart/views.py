97be5d440ba3b6e73cb33b5c1982f675416a915c
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-12-13 21:57:04 +0000

    added the quantity_input_script

--- chart/views.py
+++ chart/views.py
@@ -12,12 +12,24 @@ def add_to_chart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     chart = request.session.get('chart', {})
 
-    if item_id in list(chart.keys()):
-        chart[item_id] += quantity
+    if size:
+        if item_id in list(chart.keys()):
+            if size in chart[item_id]['items_by_size'].keys():
+                chart[item_id]['items_by_size'][size] += quantity
+            else:
+                chart[item_id]['items_by_size'][size] = quantity
+        else:
+            chart[item_id] = {'items_by_size': {size: quantity}}
     else:
-        chart[item_id] = quantity
+        if item_id in list(chart.keys()):
+            chart[item_id] += quantity
+        else:
+            chart[item_id] = quantity
 
     request.session['chart'] = chart
     return redirect(redirect_url)
