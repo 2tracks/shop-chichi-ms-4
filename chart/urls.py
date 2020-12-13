7524446d2e948095b315388e6bbff85576bdf555
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-12-13 23:19:22 +0000

    added adjust/remove chart to view and calc subtotal template filter

--- chart/urls.py
+++ chart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_chart, name='view_chart'),
     path('add/<item_id>/', views.add_to_chart, name='add_to_chart'),
+    path('adjust/<item_id>/', views.adjust_chart, name='adjust_chart'),
+    path('remove/<item_id>/', views.remove_from_chart, name='remove_from_chart'),
 ]
--- chart/views.py
+++ chart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_chart(request):
@@ -33,3 +33,52 @@ def add_to_chart(request, item_id):
 
     request.session['chart'] = chart
     return redirect(redirect_url)
+
+
+def adjust_chart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    chart = request.session.get('chart', {})
+
+    if size:
+        if quantity > 0:
+            chart[item_id]['items_by_size'][size] = quantity
+        else:
+            del chart[item_id]['items_by_size'][size]
+            if not chart[item_id]['items_by_size']:
+                chart.pop(item_id)
+    else:
+        if quantity > 0:
+            chart[item_id] = quantity
+        else:
+            chart.pop(item_id)
+
+    request.session['chart'] = chart
+    return redirect(reverse('view_chart'))
+
+
+def remove_from_chart(request, item_id):
+    """Remove item from the shopping chart"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        chart = request.session.get('chart', {})
+
+        if size:
+            del chart[item_id]['items_by_size'][size]
+            if not chart[item_id]['items_by_size']:
+                chart.pop(item_id)
+        else:
+            chart.pop(item_id)
+
+        request.session['chart'] = chart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
